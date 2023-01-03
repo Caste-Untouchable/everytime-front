167d11967d32813871f4f647d70209bc2e68874a
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2023-01-03 19:56:10 +0900

    chore: 에브리타임 컬러 코드 추가
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- lib/const.dart
+++ lib/const.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
 class EveryTimeColor {
+  static const bulb = Color(0xFFFFCC1C);
   static const red = Color(0xFFB33423);
+  static const todo = Color(0xFF88BAFF);
 }
