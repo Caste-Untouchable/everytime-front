871f500bf2ffc2d0a2fece936a501ed8165cbeea
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2023-01-05 02:32:24 +0900

    feat: 에브리타임 red color 변경
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- lib/const.dart
+++ lib/const.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class EveryTimeColor {
   static const bulb = Color(0xFFFFCC1C);
   static const cyan = Color(0xFF0CA5AF);
-  static const red = Color(0xFFB33423);
+  static const red = Color(0xFFB81918);
   static const todo = Color(0xFF88BAFF);
 }
