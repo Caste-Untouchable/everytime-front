9059b6bcf6fbe0b6143a764f0c2047f61a3850b4
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2023-01-07 04:54:03 +0900

    fix: 잘못된 secure_storage 패키지 수정
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,6 @@ import flutter_secure_storage_macos
 import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FlutterSecureStorageMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageMacosPlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
 }
