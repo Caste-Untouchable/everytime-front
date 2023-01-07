c621f08945a3a58a6022a28ce749343975380ed0
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2023-01-07 18:12:52 +0900

    feat: 게시글 API 적용
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- lib/screens/board/politics_board.dart
+++ lib/screens/board/politics_board.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+
+import 'package:clone_everytime/models/article.dart';
+import 'package:clone_everytime/screens/board/board_detail.dart';
 import 'package:clone_everytime/screens/board/politics_board_search.dart';
 import 'package:clone_everytime/screens/board/politics_board_write.dart';
-import 'package:clone_everytime/screens/board/board_detail.dart';
+import 'package:clone_everytime/utils/database/every_time_api.dart';
+import 'package:clone_everytime/utils/date_util.dart';
 
 enum SampleItem { itemOne, itemTwo, itemThree }
 
 class PoliticsBoard extends StatefulWidget {
-  const PoliticsBoard({super.key});
+  const PoliticsBoard({super.key, required this.boardTitle, required this.boardTypeId, required this.jwt});
+
+  final String boardTitle;
+  final int boardTypeId;
+  final String jwt;
 
   @override
   State<PoliticsBoard> createState() => _PoliticsBoardState();
@@ -21,12 +29,12 @@ class _PoliticsBoardState extends State<PoliticsBoard> {
         toolbarHeight: 60,
         backgroundColor: Colors.white,
         foregroundColor: Colors.black,
-        title: Column(crossAxisAlignment: CrossAxisAlignment.start, mainAxisSize: MainAxisSize.min, children: const [
-          Text('시사·이슈', style: TextStyle(fontSize: 12, fontWeight: FontWeight.w900)),
-          SizedBox(
+        title: Column(crossAxisAlignment: CrossAxisAlignment.start, mainAxisSize: MainAxisSize.min, children: [
+          Text(widget.boardTitle, style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w900)),
+          const SizedBox(
             height: 3,
           ),
-          Text('동의대', style: TextStyle(fontSize: 12, fontWeight: FontWeight.w100, color: Color.fromARGB(255, 142, 141, 141)))
+          const Text('동의대', style: TextStyle(fontSize: 12, fontWeight: FontWeight.w100, color: Color.fromARGB(255, 142, 141, 141)))
         ]),
         actions: [
           IconButton(
@@ -75,7 +83,7 @@ class _PoliticsBoardState extends State<PoliticsBoard> {
       ),
       body: Stack(
         children: [
-          const PoliticsDB(),
+          PoliticsDB(jwt: widget.jwt),
           Align(
             alignment: Alignment.bottomCenter,
             child: Padding(
@@ -114,290 +122,146 @@ class _PoliticsBoardState extends State<PoliticsBoard> {
   }
 }
 
-const title = [
-  "기이이이이인아속보12345이ㅗㄴㅁ옿ㄴㅁ오ㅓㅁㄴㅇㅁㅇ넘ㅇ넘ㄴ엄ㄴ어ㅏ",
-  "속보2",
-  "기이이이이인 속보1234",
-  "기이이이이인 속보기이이이이인 속보아ㅇㄴㄴㅁㅇㅇㄴㅁ",
-  "속보1",
-  "속보2",
-  "속보3",
-  "기이이이이인 속보",
-  "기이이이이인아속보12345이ㅗㄴㅁ옿ㄴㅁ오ㅓㅁㄴㅇㅁㅇ넘ㅇ넘ㄴ엄ㄴ어ㅏ",
-  "속보2",
-  "기이이이이인 속보1234",
-  "기이이이이인 속보기이이이이인 속보아ㅇㄴㄴㅁㅇㅇㄴㅁ",
-  "속보1",
-  "속보2",
-  "속보3",
-  "기이이이이인 속보",
-];
-const text = [
-  "유익한 정보1",
-  "유익한 정보2",
-  "유익한 정보3ㅁ옿ㅁㄴ온몬ㅁ온너ㅗㅎㅇㄴㅁㄹ욪뎌ㅓ점ㄴㅇㅎㅂㅈ",
-  "기이이인 정보",
-  "유익한 정보1",
-  "유익한 정보2",
-  "유익한 정보3",
-  "기이이인 정보",
-  "유익한 정보1",
-  "유익한 정보2",
-  "유익한 정보3ㅁ옿ㅁㄴ온몬ㅁ온너ㅗㅎㅇㄴㅁㄹ욪뎌ㅓ점ㄴㅇㅎㅂㅈ",
-  "기이이인 정보",
-  "유익한 정보1",
-  "유익한 정보2",
-  "유익한 정보3",
-  "기이이인 정보",
-];
-const date = [
-  "01/02",
-  "01/02",
-  "01/02",
-  "01/02",
-  "01/02",
-  "01/02",
-  "01/02",
-  "01/02",
-  "01/02",
-  "01/02",
-  "01/02",
-  "01/02",
-  "01/02",
-  "01/02",
-  "01/02",
-  "01/02",
-];
-const name = [
-  "익명",
-  "익명",
-  "익명",
-  "익명",
-  "익명",
-  "익명",
-  "익명",
-  "익명",
-  "익명",
-  "익명",
-  "익명",
-  "익명",
-  "익명",
-  "익명",
-  "익명",
-  "익명",
-];
-const picture = [
-  "assets/images/test.jpg",
-  null,
-  "assets/icons/icn_m_search_gray800.png",
-  null,
-  "assets/images/test.jpg",
-  null,
-  "assets/icons/icn_m_search_gray800.png",
-  null,
-  "assets/images/test.jpg",
-  null,
-  "assets/icons/icn_m_search_gray800.png",
-  null,
-  "assets/images/test.jpg",
-  null,
-  "assets/icons/icn_m_search_gray800.png",
-  null,
-];
-const comment = [
-  "4",
-  "2",
-  "1",
-  "2",
-  "4",
-  "2",
-  "1",
-  "2",
-  "4",
-  "2",
-  "1",
-  "2",
-  "4",
-  "2",
-  "1",
-  "2",
-];
-const good = [
-  "3",
-  "2",
-  "1",
-  "4",
-  "3",
-  "2",
-  "1",
-  "4",
-  "3",
-  "2",
-  "1",
-  "4",
-  "3",
-  "2",
-  "1",
-  "4",
-];
-const image_index = [
-  "1",
-  "0",
-  "1",
-  "0",
-  "1",
-  "0",
-  "1",
-  "0",
-  "1",
-  "0",
-  "1",
-  "0",
-  "1",
-  "0",
-  "1",
-  "0",
-];
-
 class PoliticsDB extends StatelessWidget {
-  const PoliticsDB({super.key});
+  PoliticsDB({super.key, required this.jwt});
+
+  String jwt;
+
+  Future<List<Article>> getArticle() async {
+    List<Article> articleList = await EveryTimeApi.getBoardArticle(1, jwt);
+
+    return articleList;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      itemCount: title.length,
-      itemBuilder: (BuildContext context, index) {
-        return InkWell(
-          onTap: () {
-            Navigator.push(context, MaterialPageRoute(builder: ((context) => DetailScreen())));
-          },
-          child: Container(
-            padding: const EdgeInsets.only(left: 15, right: 15),
-            height: 60,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded(
-                  child: SizedBox(
-                    width: double.infinity,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
+    return FutureBuilder(
+        future: getArticle(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasData) {
+            return ListView.separated(
+              itemCount: snapshot.data.length,
+              itemBuilder: (BuildContext context, index) {
+                return InkWell(
+                  onTap: () {
+                    Navigator.push(context, MaterialPageRoute(builder: ((context) => const DetailScreen())));
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.only(left: 15, right: 15),
+                    height: 60,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(
-                          title[index],
-                          overflow: TextOverflow.ellipsis,
-                          style: const TextStyle(fontSize: 16, color: Colors.black),
-                        ),
-                        const SizedBox(
-                          height: 5,
-                        ),
-                        Text(
-                          text[index],
-                          overflow: TextOverflow.ellipsis,
-                          style: const TextStyle(fontSize: 12, color: Color.fromARGB(255, 142, 141, 141)),
-                        ),
-                        const SizedBox(
-                          height: 5,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: [
-                            Row(children: [
-                              Text(
-                                date[index],
-                                style: const TextStyle(fontSize: 12, color: Color.fromARGB(255, 172, 171, 171)),
-                              ),
-                              const Text(
-                                ' | ',
-                                style: TextStyle(fontSize: 12, color: Color.fromARGB(255, 209, 209, 209)),
-                              ),
-                              Text(
-                                name[index],
-                                style: const TextStyle(fontSize: 12, color: Color.fromARGB(255, 172, 171, 171)),
-                              ),
-                            ]),
-                            Row(
+                        Expanded(
+                          child: SizedBox(
+                            width: double.infinity,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                if (picture[index] != null) const ImageIcon(AssetImage("assets/icons/icn_s_image_gray500.png"), size: 12),
-                                if (picture[index] != null)
-                                  const Text(
-                                    "  ",
-                                    style: TextStyle(fontSize: 12),
-                                  ),
-                                if (picture[index] != null)
-                                  Text(
-                                    image_index[index],
-                                    style: const TextStyle(fontSize: 12, color: Colors.grey),
-                                  ),
-                                const Text(
-                                  "  ",
-                                  style: TextStyle(fontSize: 12),
+                                Text(
+                                  snapshot.data[index].boardTitle!,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: const TextStyle(fontSize: 16, color: Colors.black),
                                 ),
-                                const ImageIcon(
-                                    color: Color.fromARGB(255, 209, 31, 19),
-                                    AssetImage(
-                                      "assets/icons/icn_s_posvote_red.png",
-                                    ),
-                                    size: 12),
-                                const Text(
-                                  " ",
-                                  style: TextStyle(fontSize: 12),
+                                const SizedBox(
+                                  height: 5,
                                 ),
                                 Text(
-                                  comment[index],
-                                  style: const TextStyle(fontSize: 12, color: Color.fromARGB(255, 209, 31, 19)),
+                                  snapshot.data[index].content!,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: const TextStyle(fontSize: 12, color: Color.fromARGB(255, 142, 141, 141)),
                                 ),
-                                const Text(
-                                  "  ",
-                                  style: TextStyle(fontSize: 12),
+                                const SizedBox(
+                                  height: 5,
                                 ),
-                                const ImageIcon(
-                                    color: Color.fromARGB(255, 12, 165, 175),
-                                    AssetImage(
-                                      "assets/icons/icn_s_comment_cyan.png",
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  children: [
+                                    Row(children: [
+                                      Text(
+                                        DateUtil.dateToString(snapshot.data[index].createdAT!),
+                                        style: const TextStyle(fontSize: 12, color: Color.fromARGB(255, 172, 171, 171)),
+                                      ),
+                                      const Text(
+                                        ' | ',
+                                        style: TextStyle(fontSize: 12, color: Color.fromARGB(255, 209, 209, 209)),
+                                      ),
+                                      Text(
+                                        snapshot.data[index].nickname!,
+                                        style: const TextStyle(fontSize: 12, color: Color.fromARGB(255, 172, 171, 171)),
+                                      ),
+                                    ]),
+                                    Row(
+                                      children: [
+                                        const Text(
+                                          "  ",
+                                          style: TextStyle(fontSize: 12),
+                                        ),
+                                        const ImageIcon(
+                                            color: Color.fromARGB(255, 209, 31, 19),
+                                            AssetImage(
+                                              "assets/icons/icn_s_posvote_red.png",
+                                            ),
+                                            size: 12),
+                                        const Text(
+                                          " ",
+                                          style: TextStyle(fontSize: 12),
+                                        ),
+                                        Text(
+                                          snapshot.data[index].recommendCount!.toString(),
+                                          style: const TextStyle(fontSize: 12, color: Color.fromARGB(255, 209, 31, 19)),
+                                        ),
+                                        const Text(
+                                          "  ",
+                                          style: TextStyle(fontSize: 12),
+                                        ),
+                                        const ImageIcon(
+                                            color: Color.fromARGB(255, 12, 165, 175),
+                                            AssetImage(
+                                              "assets/icons/icn_s_comment_cyan.png",
+                                            ),
+                                            size: 12),
+                                        const Text(
+                                          " ",
+                                          style: TextStyle(fontSize: 12),
+                                        ),
+                                        const Text(
+                                          '7',
+                                          style: TextStyle(fontSize: 12, color: Color.fromARGB(255, 12, 165, 175)),
+                                        ),
+                                        const Text(
+                                          "    ",
+                                          style: TextStyle(fontSize: 12),
+                                        ),
+                                      ],
                                     ),
-                                    size: 12),
-                                const Text(
-                                  " ",
-                                  style: TextStyle(fontSize: 12),
-                                ),
-                                Text(
-                                  comment[index],
-                                  style: const TextStyle(fontSize: 12, color: Color.fromARGB(255, 12, 165, 175)),
-                                ),
-                                const Text(
-                                  "    ",
-                                  style: TextStyle(fontSize: 12),
-                                ),
+                                  ],
+                                )
                               ],
                             ),
-                          ],
-                        )
+                          ),
+                        ),
                       ],
                     ),
+                    //Text(title[index]),
                   ),
-                ),
-                if (picture[index] != null)
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(10.0),
-                    child: Image.asset(picture[index]!),
-                  )
-                //ImageIcon(AssetImage(picture[index]!), size: 70),
-              ],
-            ),
-            //Text(title[index]),
-          ),
-        );
-      },
-      separatorBuilder: (BuildContext context, int index) {
-        return const Padding(
-          padding: EdgeInsets.fromLTRB(10.0, 0, 10.0, 0),
-          child: Divider(color: Color.fromARGB(255, 194, 194, 194), thickness: 1),
-        );
-      },
-    );
+                );
+              },
+              separatorBuilder: (BuildContext context, int index) {
+                return const Padding(
+                  padding: EdgeInsets.fromLTRB(10.0, 0, 10.0, 0),
+                  child: Divider(color: Color.fromARGB(255, 194, 194, 194), thickness: 1),
+                );
+              },
+            );
+          } else {
+            return const Center(child: Text("게시글이 없습니다."));
+          }
+        });
   }
 }
