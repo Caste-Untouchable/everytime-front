79e15e26a71117a3cebd09df5c42e206b176b80f
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2023-01-08 04:35:05 +0900

    chore: TextField에 focus시 흰색 변경 구현
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- lib/widgets/everytime_widgets.dart
+++ lib/widgets/everytime_widgets.dart
@@ -35,7 +35,7 @@ class EverytimeButton extends StatelessWidget {
   }
 }
 
-class EverytimeTextField extends StatelessWidget {
+class EverytimeTextField extends StatefulWidget {
   EverytimeTextField({super.key, required this.title, required this.controller, this.isObscure = false, this.hint});
 
   String title;
@@ -44,46 +44,60 @@ class EverytimeTextField extends StatelessWidget {
   bool isObscure;
 
   @override
+  State<EverytimeTextField> createState() => _EverytimeTextFieldState();
+}
+
+class _EverytimeTextFieldState extends State<EverytimeTextField> {
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.fromLTRB(12.0, 4.0, 4.0, 4.0),
-          child: Text(
-            title,
-            style: const TextStyle(color: Colors.black54, fontWeight: FontWeight.w700, fontSize: 13.0),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 4.0, right: 4.0),
-          child: Container(
-            alignment: Alignment.center,
-            height: MediaQuery.of(context).size.height * 0.05,
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey[300]!, width: 1.0),
-              borderRadius: BorderRadius.circular(10.0),
-              color: Colors.grey[100],
-            ),
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(10.0, 8.0, 10.0, 8.0),
-              child: TextField(
-                  controller: controller,
-                  obscureText: isObscure,
-                  textAlignVertical: TextAlignVertical.center,
-                  decoration: InputDecoration(
-                      isDense: true,
-                      isCollapsed: true,
-                      border: InputBorder.none,
-                      focusedBorder: InputBorder.none,
-                      hoverColor: Colors.grey,
-                      hintText: hint,
-                      hintStyle: const TextStyle(color: Colors.grey))),
+    return FocusScope(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(12.0, 4.0, 4.0, 4.0),
+            child: Text(
+              widget.title,
+              style: const TextStyle(color: Colors.black54, fontWeight: FontWeight.w700, fontSize: 13.0),
             ),
           ),
-        ),
-        const SizedBox(height: 20.0),
-      ],
+          Padding(
+            padding: const EdgeInsets.only(left: 4.0, right: 4.0),
+            child: Focus(
+              child: Builder(builder: (context) {
+                final FocusNode focusNode = Focus.of(context);
+                final bool hasFocus = focusNode.hasFocus;
+
+                return Container(
+                  alignment: Alignment.center,
+                  height: MediaQuery.of(context).size.height * 0.05,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey[300]!, width: 1.0),
+                    borderRadius: BorderRadius.circular(10.0),
+                    color: hasFocus ? Colors.white : Colors.grey[100],
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(10.0, 8.0, 10.0, 8.0),
+                    child: TextField(
+                        controller: widget.controller,
+                        obscureText: widget.isObscure,
+                        textAlignVertical: TextAlignVertical.center,
+                        decoration: InputDecoration(
+                            isDense: true,
+                            isCollapsed: true,
+                            border: InputBorder.none,
+                            focusedBorder: InputBorder.none,
+                            hoverColor: Colors.grey,
+                            hintText: widget.hint,
+                            hintStyle: const TextStyle(color: Colors.grey))),
+                  ),
+                );
+              }),
+            ),
+          ),
+          const SizedBox(height: 20.0),
+        ],
+      ),
     );
   }
 }
