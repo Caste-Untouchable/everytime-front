c59f4c270ee1c993448fc085d4927e0908562abf
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2023-01-07 22:55:01 +0900

    fix: 날짜 변환 오류 수정
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- lib/utils/date_util.dart
+++ lib/utils/date_util.dart
@@ -1,15 +1,17 @@
 import 'package:intl/intl.dart';
 
 class DateUtil {
-  static String dateToString(DateTime createdAt) {
-    Duration timeDiff = createdAt.difference(DateTime.now());
+  static String boardTimeToString(DateTime createdAt) {
+    Duration timeDiff = DateTime.now().difference(createdAt);
 
-    if (timeDiff.inMinutes < 600) {
-      return DateFormat('mm분전').format(createdAt);
+    createdAt = createdAt.add(const Duration(hours: 9));
+
+    if (timeDiff.inMinutes < 60) {
+      return '$timeDiff분전';
     } else if (timeDiff.inDays > 0) {
       return DateFormat('MM/dd').format(createdAt);
+    } else {
+      return DateFormat('HH:mm').format(createdAt);
     }
-
-    return createdAt.toString();
   }
 }
